b7a9318f5f85f794270e1e8cc684352db7c1370f
PatilShubham22 <shubham22398patil@gmail.com>
2022-09-09 19:44:50 +0530

    Added New Files

---
+++ Basic/01.Output Function - cout.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 15, No2 = 20, Sum = 0;
+
+    Sum = No1 + No2;
+
+    cout << endl << " *************************** ";
+    cout << endl << " Addition of " << No1 << " & " << No2 << " is = " << Sum << endl;
+    cout << " *************************** ";
+
+    cout << endl << " Thanks...";
+
+    getch();
+    return 0;
+}
---
+++ Basic/02. Input Output Function - cout & cin.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0, No2 = 0, Sum = 0;
+
+    cout << " Enter 1st Number = ";
+    cin >> No1;
+
+    cout << endl << " Enter 2nd Number = ";
+    cin >> No2;
+
+    Sum = No1 + No2;
+
+    cout << endl << " *************************** ";
+    cout << endl << " Addition of " << No1 << " & " << No2 << " is = " << Sum << "."<< endl;
+    cout << " *************************** ";
+
+    return 0;
+}
